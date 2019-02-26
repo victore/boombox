174471db9181b69fda18e16d9abe15203370296f
Victor Guadarrama <victor.gm@eucledian.com>
2019-02-26 09:49:56 -0600

    First Component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'My Minimal React Webpack Babel Setup';
+const tracks = [
+  {
+    title: 'Some track'
+  },
+  {
+    title: 'Some other track'
+  }
+];
 
 ReactDOM.render(
-  <div>{title}</div>,
+   <div>
+    {
+      tracks.map((track) => {
+        return <div className="track">{track.title}</div>;
+      })
+    }
+  </div>,
   document.getElementById('app')
 );
 
